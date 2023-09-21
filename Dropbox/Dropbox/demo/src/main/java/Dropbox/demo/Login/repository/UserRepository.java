f29bad4c16a8cced3fe4790f8708f0db0e560b91
aayush010 <jaiswalaayush6@gmail.com>
2023-09-21 16:21:26 +0530

    Delete Dropbox/Dropbox directory

--- Dropbox/Dropbox/demo/src/main/java/Dropbox/demo/Login/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package Dropbox.demo.Login.repository;
-
-import Dropbox.demo.Login.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByEmail(String email);
-}
--- Dropbox/Dropbox/demo/src/main/java/Dropbox/demo/StorageService/repository/UserDataRepository.java
+++
@@ -1,9 +0,0 @@
-package Dropbox.demo.StorageService.repository;
-
-import Dropbox.demo.StorageService.Model.FileInfo;
-import Dropbox.demo.StorageService.Model.UserDataInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDataRepository extends JpaRepository<UserDataInfo, Long> {
-
-}
